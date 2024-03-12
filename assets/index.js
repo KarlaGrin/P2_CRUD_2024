ce42fb2362fde9436ce6878db224a86933fbdd7a
KarlaGrin <karlagrinen@gmail.com>
2024-03-11 22:40:25 -0300

    batallando con CSS

--- assets/index.js
+++ assets/index.js
@@ -8,79 +8,79 @@ let editingIndex = -1;
 
 cargarPendientesDesdeLocalStorage();
 
-crearButton.addEventListener('click', function() {
-  const tarea = textarea.value.trim();
-  if (tarea !== '') {
-    if (editingIndex === -1) {
-      agregarPendiente(tarea);
-    } else {
-      editarPendiente(editingIndex, tarea);
-      editarButton.style.display = 'none';
-      crearButton.style.display = 'block';
-      textarea.value = '';
-      editingIndex = -1;
+crearButton.addEventListener('click', function () {
+    const tarea = textarea.value.trim();
+    if (tarea !== '') {
+        if (editingIndex === -1) {
+            agregarPendiente(tarea);
+        } else {
+            editarPendiente(editingIndex, tarea);
+            editarButton.style.display = 'none';
+            crearButton.style.display = 'block';
+
+            editingIndex = -1;
+        }
+        guardarPendientesEnLocalStorage();
+        textarea.value = '';
+
     }
-    guardarPendientesEnLocalStorage();
-    
-    /*addTextArea.value = ''; //test para arreglar revision de proyecto , Paulo uso en la linea 39 addUserInput.value = ''; */
-  }
 });
 
-borrarTextoButton.addEventListener('click', function() {
-  textarea.value = '';
-});
+/*borrarTextoButton.addEventListener('click', function () {
+    textarea.value = '';
+}); */
 
 function agregarPendiente(tarea) {
-  const li = document.createElement('li');
-  li.innerHTML = `
+    const li = document.createElement('li');
+    li.innerHTML = `
     <span>${tarea}</span>
     <button class="editarTarea">Editar</button>
     <button class="borrarTarea">Borrar</button>
   `;
-  pendientesList.appendChild(li);
+    pendientesList.appendChild(li);
 
-  const editarTareaButton = li.querySelector('.editarTarea');
-  const borrarTareaButton = li.querySelector('.borrarTarea');
+    const editarTareaButton = li.querySelector('.editarTarea');
+    const borrarTareaButton = li.querySelector('.borrarTarea');
 
-  editarTareaButton.addEventListener('click', function() {
-    editarButton.style.display = 'block';
-    crearButton.style.display = 'none';
-    textarea.value = li.querySelector('span').textContent;
-    editingIndex = Array.from(pendientesList.children).indexOf(li);
-  });
+    editarTareaButton.addEventListener('click', function () {
+        editarButton.style.display = 'block';
+        crearButton.style.display = 'none';
+        textarea.value = li.querySelector('span').textContent;
+        editingIndex = Array.from(pendientesList.children).indexOf(li);
+    });
 
-  borrarTareaButton.addEventListener('click', function() {
-    pendientesList.removeChild(li);
-    guardarPendientesEnLocalStorage(); 
-  });
+    borrarTareaButton.addEventListener('click', function () {
+        pendientesList.removeChild(li);
+        guardarPendientesEnLocalStorage();
+    });
 }
 
 function editarPendiente(index, nuevaTarea) {
-  const li = pendientesList.children[index];
-  li.querySelector('span').textContent = nuevaTarea;
-  guardarPendientesEnLocalStorage(); 
+    const li = pendientesList.children[index];
+    li.querySelector('span').textContent = nuevaTarea;
+    guardarPendientesEnLocalStorage();
 }
 
-editarButton.addEventListener('click', function() {
-  const nuevaTarea = textarea.value.trim();
-  if (nuevaTarea !== '') {
-    editarPendiente(editingIndex, nuevaTarea);
-    editarButton.style.display = 'none';
-    crearButton.style.display = 'block';
-    textarea.value = '';
-    editingIndex = -1;
-  }
+editarButton.addEventListener('click', function () {
+    const nuevaTarea = textarea.value.trim();
+    if (nuevaTarea !== '') {
+        editarPendiente(editingIndex, nuevaTarea);
+        editarButton.style.display = 'none';
+        crearButton.style.display = 'block';
+        textarea.value = '';
+        editingIndex = -1;
+    }
 });
 
 function cargarPendientesDesdeLocalStorage() {
-  const pendientes = JSON.parse(localStorage.getItem('pendientes')) || [];
-  pendientes.forEach(pendiente => {
-    agregarPendiente(pendiente);
-  });
+    const pendientes = JSON.parse(localStorage.getItem('pendientes')) || [];
+    pendientes.forEach(pendiente => {
+        agregarPendiente(pendiente);
+    });
 }
 
 function guardarPendientesEnLocalStorage() {
-  const pendientes = Array.from(pendientesList.children).map(li => li.querySelector('span').textContent);
-  localStorage.setItem('pendientes', JSON.stringify(pendientes));
+    const pendientes = Array.from(pendientesList.children).map(li => li.querySelector('span').textContent);
+    localStorage.setItem('pendientes', JSON.stringify(pendientes));
 }
 
